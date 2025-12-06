21f066f1a115e5ac6925f1d9861bdd132464a372
Zachary Chavez <zpchavez@gmail.com>
2025-12-06 16:46:02 -0700

    Add some styling to the nav links

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,4 +1,5 @@
 import { Link } from "@tanstack/react-router";
+import { MainSearchBar } from "./MainSearchBar";
 
 type NavLinkProps = {
   path: string;
@@ -6,11 +7,18 @@ type NavLinkProps = {
 };
 
 function NavLink({ path, text }: NavLinkProps) {
+  const baseClasses = "px-4 py-2 text-white! rounded-lg no-underline";
+  const activeClasses = "bg-blue-600 hover:bg-blue-500";
+  const inactiveClasses = "bg-gray-700 hover:bg-gray-600";
+
   return (
     <Link
       to={path}
       activeProps={{
-        className: "button--color--primary",
+        className: `${baseClasses} ${activeClasses}`,
+      }}
+      inactiveProps={{
+        className: `${baseClasses} ${inactiveClasses}`,
       }}
     >
       {text}
@@ -20,32 +28,19 @@ function NavLink({ path, text }: NavLinkProps) {
 
 export function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="main-layout container">
-      <h1 className="site-title">Comic Strip Library</h1>
-      <div className="searchbar">
-        {/* @TODO search bar goes here */}
-        {/* <form
-          onSubmit={handleSubmit(this.handleSubmit)}
-          className="input-scaffold"
-        >
-          <Field
-            name="query"
-            label="Search"
-            component={ReduxFormsField}
-            required
-          >
-            <input id="searchbar" type="text" />
-          </Field>
-        </form> */}
-      </div>
-      <nav>
-        <NavLink path="/" text="News" />
-        <NavLink path="/browse" text="Browse" />
-        <NavLink path="/links" text="Books & Links" />
-        <NavLink path="/about" text="About" />
-        <NavLink path="/copyright" text="Copyright Statement" />
-        <NavLink path="/contact" text="Contact" />
-      </nav>
+    <div>
+      <header className="text-center mb-8">
+        <h1 className="site-title">Comic Strip Library</h1>
+        <MainSearchBar />
+        <nav className="flex flex-wrap justify-center gap-3 mt-4">
+          <NavLink path="/" text="News" />
+          <NavLink path="/browse" text="Browse" />
+          <NavLink path="/links" text="Books & Links" />
+          <NavLink path="/about" text="About" />
+          <NavLink path="/copyright" text="Copyright Statement" />
+          <NavLink path="/contact" text="Contact" />
+        </nav>
+      </header>
       <div className="main-layout__children paper">{children}</div>
     </div>
   );
---
+++ src/components/MainSearchBar.tsx
@@ -0,0 +1,3 @@
+export function MainSearchBar() {
+  return <div>Search Bar Goes Here</div>;
+}
