e6b7f4740c2121e70fa7cebcd644f410a5b959d3
Zachary Chavez <zpchavez@gmail.com>
2025-12-07 20:15:28 -0700

    Add about page

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -10,11 +10,17 @@
 
 import { Route as rootRouteImport } from './routes/__root'
 import { Route as LinksRouteImport } from './routes/links'
+import { Route as AboutRouteImport } from './routes/about'
 import { Route as IndexRouteImport } from './routes/index'
 
 const LinksRoute = LinksRouteImport.update({
   id: '/links',
   path: '/links',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const AboutRoute = AboutRouteImport.update({
+  id: '/about',
+  path: '/about',
   getParentRoute: () => rootRouteImport,
 } as any)
 const IndexRoute = IndexRouteImport.update({
@@ -25,27 +31,31 @@ const IndexRoute = IndexRouteImport.update({
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '/about': typeof AboutRoute
   '/links': typeof LinksRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '/about': typeof AboutRoute
   '/links': typeof LinksRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/': typeof IndexRoute
+  '/about': typeof AboutRoute
   '/links': typeof LinksRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/links'
+  fullPaths: '/' | '/about' | '/links'
   fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/links'
-  id: '__root__' | '/' | '/links'
+  to: '/' | '/about' | '/links'
+  id: '__root__' | '/' | '/about' | '/links'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  AboutRoute: typeof AboutRoute
   LinksRoute: typeof LinksRoute
 }
 
@@ -56,6 +66,13 @@ declare module '@tanstack/react-router' {
       path: '/links'
       fullPath: '/links'
       preLoaderRoute: typeof LinksRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/about': {
+      id: '/about'
+      path: '/about'
+      fullPath: '/about'
+      preLoaderRoute: typeof AboutRouteImport
       parentRoute: typeof rootRouteImport
     }
     '/': {
@@ -70,6 +87,7 @@ declare module '@tanstack/react-router' {
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  AboutRoute: AboutRoute,
   LinksRoute: LinksRoute,
 }
 export const routeTree = rootRouteImport
---
+++ src/routes/about.tsx
@@ -0,0 +1,31 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/about")({
+  component: AboutRoute,
+});
+
+function AboutRoute() {
+  return (
+    <div className="w-1/2 mx-auto">
+      <h2 className="text-center">About the Comic Strip Library</h2>
+      <p className="paragraph">
+        The Comic Strip Library is a digital library containing a collection of
+        public domain comic strips. Everything in the collection predates 1923
+        and is no longer under copyright in the United States. Unlike many comic
+        databases which are indexed by date alone, the comics in the Comic Strip
+        Library are thoroughly cataloged. Searches on the Comic Strip Library
+        can match text from dialogue and narration; character names; subject
+        matter; objects, creatures, and people depicted in a strip; or even
+        literary techniques. For example, a search for "fourth wall" will turn
+        up comic strips which break the fourth wall, and a search for "puns"
+        will bring up comics containing word play.
+      </p>
+      <p className="paragraph">
+        The Comic Strip Library began as a class project. I continued working on
+        it for many years after graduating in order to improve my web
+        development skills. Now it exists in a read-only form for archival
+        purposes.
+      </p>
+    </div>
+  );
+}
