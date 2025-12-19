cd69987c0b1593fea0423c6c2a15573216807309
Zachary Chavez <zpchavez@gmail.com>
2025-12-19 11:41:21 -0700

    Update loading indicator

--- src/components/LoadingIndicator.tsx
+++ src/components/LoadingIndicator.tsx
@@ -1,4 +1,24 @@
-export function LoadingIndicator() {
-  // @TODO replace with actual spinner. Center it.
-  return <div>Loading...</div>;
+import { useEffect, useState } from "react";
+import { Spinner } from "src/components/ui/spinner";
+
+type LoadingIndicatorProps = {
+  delay?: number;
+};
+
+export function LoadingIndicator({ delay = 300 }: LoadingIndicatorProps) {
+  const [delayElapsed, setDelayElapsed] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDelayElapsed(true);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [delay]);
+
+  return (
+    <div className="flex justify-center">
+      {delayElapsed && <Spinner className="size-10" />}
+    </div>
+  );
 }
